df7a6d1c65e6a5c9838780ec1e49eb4bc3a9899b
Subash <subashsuni55@gmail.com>
2026-03-24 14:24:26 +0530

    added pricing page

---
+++ src/components/pages/pricing/hero.tsx
@@ -0,0 +1,16 @@
+const Hero = () => {
+	return (
+		<section className="mb-20 text-center max-w-3xl mx-auto">
+			<h1 className="font-headline font-bold text-5xl md:text-6xl text-(--on-surface) tracking-tight mb-6">
+				Simple pricing for{" "}
+				<span className="text-(--primary) italic">extraordinary</span> service.
+			</h1>
+			<p className="text-(--on-surface-variant) text-lg md:text-xl font-body">
+				Choose the perfect menu management tier for your restaurant. From local
+				bistros to global franchises.
+			</p>
+		</section>
+	);
+};
+
+export default Hero;
--- src/routes/pricing.tsx
+++ src/routes/pricing.tsx
@@ -1,9 +1,10 @@
 import { createFileRoute } from "@tanstack/react-router";
+import PricingPage from "#/components/pages/pricing";
 
 export const Route = createFileRoute("/pricing")({
 	component: RouteComponent,
 });
 
 function RouteComponent() {
-	return <div>Hello "/pricing"!</div>;
+	return <PricingPage />;
 }
